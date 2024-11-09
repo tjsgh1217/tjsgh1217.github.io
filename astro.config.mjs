ddeee24dddc2f074d798ffc2ec04d38b153a8be5
김선호 <kimseonho@Seonho-MacBook-Pro.local>
2024-11-09 12:31:16 +0900

    first commit

--- astro.config.mjs
+++ astro.config.mjs
@@ -8,4 +8,6 @@ import sitemap from '@astrojs/sitemap';
 export default defineConfig({
   site: 'https://example.com',
   integrations: [mdx(), sitemap()],
+  site: 'https://tjsgh1217.github.io',
+  base: 'devseon-blog',
 });
