c4e0ee451b5b44a9a0fa5b9816a5f0095dcc1e34
김선호 <kimseonho@Seonho-MacBook-Pro.local>
2024-11-09 13:07:22 +0900

    git page5

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,6 +7,6 @@ import sitemap from '@astrojs/sitemap';
 // https://astro.build/config
 export default defineConfig({
   integrations: [mdx(), sitemap()],
-  site: ' https://tjsgh1217.github.io',
+  site: ' https://tjsgh1217.github.io/development-blog',
   base: '/development-blog',
 });
