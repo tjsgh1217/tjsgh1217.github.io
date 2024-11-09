b5406737ffbe905753e66f03a56a0e3f41f262b0
김선호 <kimseonho@Seonho-MacBook-Pro.local>
2024-11-09 19:46:31 +0900

    config

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,16 +6,7 @@ import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [
-    mdx(),
-    sitemap({
-      changefreq: 'weekly', // 페이지 업데이트 빈도
-      priority: 0.5, // 페이지 우선순위
-      // exclude: ['/exclude-page'],  // 사이트맵에 제외할 페이지
-      robots: true, // robots.txt 파일 자동 생성
-      sitemapBaseFile: '/sitemap.xml', // 사이트맵 파일 기본 경로
-    }),
-  ],
+  integrations: [mdx(), sitemap()],
   site: ' https://tjsgh1217.github.io',
   // base: '/development-blog',
 });
